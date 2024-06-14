a5e00ae2fa068a259aeb90b12a878a4e281d558f
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-06-14 00:58:41 -0300

    feat: configure prisma and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-    imports: [AuthModule],
+    imports: [AuthModule, DatabaseModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
